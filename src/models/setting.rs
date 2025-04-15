c9a6592953f3db2296de988a4e6e4de53edc3f0d
keiko37 <keiko.d3v@gmail.com>
2025-04-15 17:48:19 +0300

    added plugins and settings stores, rewrited plugin data structure

---
+++ src/models/setting.rs
@@ -0,0 +1,246 @@
+use serde::{Deserialize, Serialize};
+
+use super::SettingsCategory;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Setting {
+    category: Option<SettingsCategory>,
+    title: String,
+    description: String,
+    confirmation: Option<ConfirmationWindow>,
+    is_advanced: bool,
+    component: SettingComponent,
+}
+
+impl Setting {
+    pub fn category(&self) -> &Option<SettingsCategory> {
+        &self.category
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub enum SettingComponent {
+    Switch(Switch),
+    Text(Text),
+    Number(Number),
+    ButtonGroup(ButtonGroup),
+    Select(Select),
+    MultiSelect(MultiSelect),
+    Slider(Slider)
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct ConfirmationWindow {
+    title: String,
+    description: String,
+}
+
+// impl ConfirmationWindow {
+//     pub fn new(title: String, description: String) -> ConfirmationWindow {
+//         ConfirmationWindow { title, description }
+//     }
+// }
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Switch {
+    value: bool
+}
+
+impl Switch {
+    pub fn new(value: bool) -> Switch {
+        Switch { value }
+    }
+    pub fn value(&self) -> bool {
+        self.value
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Text {
+    value: String,
+    min_length: u32,
+    max_length: u32
+}
+
+impl Text {
+    // pub fn new(setting_common: SettingCommon, value: String, min_length: u32, max_length: u32) -> Text {
+    //     Text { setting_common, value, min_length, max_length }
+    // }
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+    pub fn min_length(&self) -> u32 {
+        self.min_length
+    }
+    pub fn max_length(&self) -> u32 {
+        self.max_length
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Number {
+    value: f64,
+    min: f64,
+    max: f64,
+    step: f64,
+}
+
+impl Number {
+    // pub fn new(setting_common: SettingCommon, value: f64, min: f64, max: f64, step: f64) -> Number {
+    //     Number { setting_common, value, min, max, step }
+    // }
+    pub fn value(&self) -> f64 {
+        self.value
+    }
+    pub fn min(&self) -> f64 {
+        self.min
+    }
+    pub fn max(&self) -> f64 {
+        self.max
+    }
+    pub fn step(&self) -> f64 {
+        self.step
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct ButtonGroup {
+    value: String,
+    items: Vec<ButtonGroupItem>
+}
+
+impl ButtonGroup {
+    // pub fn new(setting_common: SettingCommon, value: String, items: Vec<ButtonGroupItem>) -> ButtonGroup {
+    //     ButtonGroup { setting_common, value, items }
+    // }
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+    pub fn items(&self) -> &Vec<ButtonGroupItem> {
+        &self.items
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct ButtonGroupItem {
+    text: String,
+    value: String,
+}
+
+impl ButtonGroupItem {
+    pub fn new(text: String, value: String) -> ButtonGroupItem {
+        ButtonGroupItem { text, value }
+    }
+    pub fn text(&self) -> &String {
+        &self.text
+    }
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Select {
+    value: String,
+    items: Vec<SelectItem>
+}
+
+impl Select {
+    // pub fn new(setting_common: SettingCommon, value: String, items: Vec<SelectItem>) -> Select {
+    //     Select { setting_common, value, items }
+    // }
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+    pub fn items(&self) -> &Vec<SelectItem> {
+        &self.items
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct SelectItem {
+    text: String,
+    value: String
+}
+
+impl SelectItem {
+    // pub fn new(text: String, value: String) -> SelectItem {
+    //     SelectItem { text, value }
+    // }
+    pub fn text(&self) -> &String {
+        &self.text
+    }
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct MultiSelect {
+    value: Vec<String>,
+    items: Vec<MultiSelectItem>
+}
+
+impl MultiSelect {
+    // pub fn new(setting_common: SettingCommon, value: Vec<String>, items: Vec<MultiSelectItem>) -> MultiSelect {
+    //     MultiSelect { setting_common, value, items }
+    // }
+    pub fn value(&self) -> &Vec<String> {
+        &self.value
+    }
+    pub fn items(&self) -> &Vec<MultiSelectItem> {
+        &self.items
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct MultiSelectItem {
+    text: String,
+    value: String
+}
+
+impl MultiSelectItem {
+    // pub fn new(text: String, value: String) -> MultiSelectItem {
+    //     MultiSelectItem { text, value }
+    // }
+    pub fn text(&self) -> &String {
+        &self.text
+    }
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Slider {
+    value: f64,
+    min: f64,
+    max: f64,
+    step: f64,
+}
+
+impl Slider {
+    // pub fn new(setting_common: SettingCommon, value: f64, min: f64, max: f64, step: f64) -> Slider {
+    //     Slider { setting_common, value, min, max, step }
+    // }
+    pub fn value(&self) -> f64 {
+        self.value
+    }
+    pub fn min(&self) -> f64 {
+        self.min
+    }
+    pub fn max(&self) -> f64 {
+        self.max
+    }
+    pub fn step(&self) -> f64 {
+        self.step
+    }
+}
