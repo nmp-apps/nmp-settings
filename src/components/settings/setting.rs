3c48c1e03016c456d1df0f50b4eb51196eff9ad3
keiko37 <81381572+keiko37@users.noreply.github.com>
2025-04-17 00:48:35 +0300

    WIP setting action components

--- src/components/settings/setting.rs
+++ src/components/settings/setting.rs
@@ -1,11 +1,13 @@
 use dioxus::{document, prelude::*};
 
+use crate::components::{ButtonGroup, Number, Select, Slider, Text, Switch};
 use crate::get_asset;
+use crate::models::SettingComponent;
 use crate::stores::StoredSetting;
 
 #[derive(PartialEq, Props, Clone)]
 pub struct SettingProps {
-    setting: StoredSetting
+    setting: ReadOnlySignal<StoredSetting>
 }
 
 /// Main Setting component
@@ -13,21 +15,45 @@ pub struct SettingProps {
 pub fn Setting(props: SettingProps) -> Element {
     let styles: String = use_hook(|| get_asset!("/assets/styles/settings/setting.css"));
 
+    let component = use_memo(move || {
+        match props.setting.read().setting().component() {
+            SettingComponent::ButtonGroup(data) => rsx! {
+                ButtonGroup {}
+            },
+            SettingComponent::MultiSelect(data) => rsx! {
+                Select {}
+            },
+            SettingComponent::Number(data) => rsx! {
+                Number {}
+            },
+            SettingComponent::Select(data) => rsx! {
+                Select {}
+            },
+            SettingComponent::Slider(data) => rsx! {
+                Slider {}
+            },
+            SettingComponent::Switch(data) => rsx! {
+                Switch {}
+            },
+            SettingComponent::Text(data) => rsx! {
+                Text {}
+            },
+        }
+    });
+
     rsx! {
         div { class: "setting",
             document::Stylesheet { href: "{styles}" }
 
             div { class: "setting__content",
 
-                h6 { {format!("{}", props.setting.setting().title())} }
-                if props.setting.setting().description().len() > 0 {
-                    p { {format!("{}", props.setting.setting().description())} }
+                h6 { {format!("{}", props.setting.read().setting().title())} }
+                if props.setting.read().setting().description().len() > 0 {
+                    p { {format!("{}", props.setting.read().setting().description())} }
                 }
             }
 
-            div { class: "setting__actions",
-                p { {format!("{:?}", props.setting.setting().component())} }
-            }
+            div { class: "setting__actions", {component} }
         }
     }
 }
