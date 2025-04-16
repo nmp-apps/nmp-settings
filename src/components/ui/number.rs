01c753938b965090d82520fd421029fe63ee3d69
keiko37 <81381572+keiko37@users.noreply.github.com>
2025-04-17 01:36:14 +0300

    styled number input

--- src/components/ui/number.rs
+++ src/components/ui/number.rs
@@ -1,6 +1,17 @@
-use dioxus::prelude::*;
+use dioxus::{document, prelude::*};
+
+use crate::get_asset;
 
 #[component]
 pub fn Number() -> Element {
-    rsx! { "Number" }
+    let styles: String = use_hook(|| get_asset!("/assets/styles/ui/number.css"));
+    rsx! {
+        div { class: "ui-number",
+            document::Stylesheet { href: "{styles}" }
+
+            button { class: "ui-number__step-button left", "<" }
+            input { class: "ui-number__input", r#type: "number" }
+            button { class: "ui-number__step-button right", ">" }
+        }
+    }
 }
