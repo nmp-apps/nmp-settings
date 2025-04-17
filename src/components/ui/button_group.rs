a77dc9eab9c901ba0b8c869fcd49fa2c2ddad3d3
keiko37 <81381572+keiko37@users.noreply.github.com>
2025-04-17 16:40:38 +0300

    changed css to rem, added button group styles

--- src/components/ui/button_group.rs
+++ src/components/ui/button_group.rs
@@ -1,6 +1,18 @@
 use dioxus::prelude::*;
 
+use crate::{components::Button, get_asset};
+
 #[component]
 pub fn ButtonGroup() -> Element {
-    rsx! { "ButtonGroup" }
+    let styles: String = use_hook(|| get_asset!("/assets/styles/ui/button_group.css"));
+
+    rsx! {
+        div { class: "ui-button-group",
+            document::Stylesheet { href: "{styles}" }
+
+            Button { "Option1" }
+            Button { "Option2" }
+            Button { "Option3" }
+        }
+    }
 }
