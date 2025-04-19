2a0cd9bbc01f6c78a2f66a470c2e5d3d8aeaae06
keiko37 <keiko.d3v@gmail.com>
2025-04-19 14:07:00 +0300

    added styles for slider component

--- src/components/ui/slider.rs
+++ src/components/ui/slider.rs
@@ -1,6 +1,30 @@
 use dioxus::prelude::*;
 
+use crate::get_asset;
+
 #[component]
 pub fn Slider() -> Element {
-    rsx! { "Slider" }
+    let styles: String = use_hook(|| get_asset!("/assets/styles/ui/slider.css"));
+    let mut value = use_signal(|| 0.0);
+    let min_value = use_signal(|| 0.0);
+    let max_value = use_signal(|| 100.0);
+    let filled_part_rem = use_memo(move || {
+        value() * 16.0 / max_value() // 16.0 is width in rem from slider's styles
+    });
+
+    rsx! {
+        document::Stylesheet { href: "{styles}" }
+        input {
+            class: "ui-slider",
+            r#type: "range",
+            style: "--private-slider-fill: {filled_part_rem}rem",
+            min: "{min_value}",
+            max: "{max_value}",
+            value: "{value}",
+            oninput: move |evt| {
+                let new_value: f64 = evt.data.value().parse().unwrap_or(0.0);
+                value.set(new_value);
+            },
+        }
+    }
 }
