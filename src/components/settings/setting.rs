ca891f158f57a4846f9459dbd23c2ee4d796dd17
keiko37 <keiko.d3v@gmail.com>
2025-04-20 04:46:23 +0300

    completed number component

--- src/components/settings/setting.rs
+++ src/components/settings/setting.rs
@@ -1,7 +1,7 @@
 use std::ops::Deref;
 use std::rc::Rc;
 
-use dioxus::logger::tracing::error;
+use dioxus::logger::tracing::{error, trace};
 use dioxus::{document, prelude::*};
 
 use crate::components::settings::setting;
@@ -21,10 +21,11 @@ pub fn Setting(props: SettingProps) -> Element {
     let styles: String = use_hook(|| get_asset!("/assets/styles/settings/setting.css"));
     let settings_store = use_context::<SettingsStore>();
 
-    let slider_handler = move |new_value: f64| {
+    let number_handler_store = settings_store.clone();
+    let number_handler = move |new_value: f64| {
         let setting_ref = props.setting.read();
         let setting = setting_ref.deref();
-        let mut categorized_settings_signal = settings_store.categorized_settings(); 
+        let mut categorized_settings_signal = number_handler_store.categorized_settings(); 
         let categorized_settings = &mut categorized_settings_signal.write();
         let target_category = setting.setting().category().as_ref().unwrap_or_else(|| {
             panic!("Target category for changing setting value is None");
@@ -32,16 +33,53 @@ pub fn Setting(props: SettingProps) -> Element {
         let category_list = categorized_settings.get_mut(&target_category);
         match category_list {
             Some(list) => {
-                let mut found_setting = list.iter_mut().find(|stored_setting| {
+                let found_setting = list.iter_mut().find(|stored_setting| {
+                    stored_setting.id() == setting.id() && stored_setting.setting().component() == setting.setting().component()
+                });
+                match found_setting {
+                    Some(stored_setting) => {
+                        match stored_setting.setting_mut().component_mut() {
+                            SettingComponent::Number(component) => {
+                                component.set_value(new_value);
+                                trace!("Number changed: new value is {}, old value is {}", new_value, component.value());
+
+                            },
+                            _ => (),
+                        }
+                    },
+                    None => {
+                        error!("Can't find setting by id {}", setting.id());
+                        return
+                    }
+                }
+            },
+            None => {
+                error!("Can't find list of settings by category \"{}\"", target_category.get_name());
+            }
+        }
+    };
+
+    let slider_handler_store = settings_store.clone();
+    let slider_handler = move |new_value: f64| {
+        let setting_ref = props.setting.read();
+        let setting = setting_ref.deref();
+        let mut categorized_settings_signal = slider_handler_store.categorized_settings(); 
+        let categorized_settings = &mut categorized_settings_signal.write();
+        let target_category = setting.setting().category().as_ref().unwrap_or_else(|| {
+            panic!("Target category for changing setting value is None");
+        });
+        let category_list = categorized_settings.get_mut(&target_category);
+        match category_list {
+            Some(list) => {
+                let found_setting = list.iter_mut().find(|stored_setting| {
                     stored_setting.id() == setting.id() && stored_setting.setting().component() == setting.setting().component()
                 });
                 match found_setting {
                     Some(stored_setting) => {
                         match stored_setting.setting_mut().component_mut() {
                             SettingComponent::Slider(component) => {
-                                println!("Slider changing: new_value is {}, old value is {}", new_value, component.value());
                                 component.set_value(new_value);
-                                println!("After slider changed: new_value is {}, old value is {}", new_value, component.value());
+                                trace!("Slider changed: new value is {}, old value is {}", new_value, component.value());
 
                             },
                             _ => (),
@@ -78,8 +116,17 @@ pub fn Setting(props: SettingProps) -> Element {
                     }
                 }
             },
-            SettingComponent::Number(data) => rsx! {
-                Number {}
+            SettingComponent::Number(data) => {
+                let handler = number_handler.clone();
+                rsx! {
+                    Number {
+                        min: data.min(),
+                        max: data.max(),
+                        step: data.step(),
+                        value: data.value(),
+                        oninput: move |new_value| handler(new_value),
+                    }
+                }
             },
             SettingComponent::Select(data) => {
                 let new_vec: Vec<SelectItem<String>> = data.items().iter().map(|item| {
@@ -105,7 +152,6 @@ pub fn Setting(props: SettingProps) -> Element {
                         step: data.step(),
                         value: data.value(),
                     }
-                    span { "{data.value()}" }
                 }
             },
             SettingComponent::Switch(data) => rsx! {
