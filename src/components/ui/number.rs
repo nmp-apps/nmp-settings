73692f032edecbaa77b50d69fce920a0cb109a83
keiko37 <81381572+keiko37@users.noreply.github.com>
2025-04-20 04:46:23 +0300

    completed number component

--- src/components/ui/number.rs
+++ src/components/ui/number.rs
@@ -1,17 +1,98 @@
+use std::ops::Deref;
+
 use dioxus::{document, prelude::*};
 
 use crate::get_asset;
 
+#[derive(PartialEq, Props, Clone)]
+pub struct NumberProps {
+    value: ReadOnlySignal<f64>,
+    min: ReadOnlySignal<f64>,
+    max: ReadOnlySignal<f64>,
+    step: ReadOnlySignal<f64>,
+    oninput: EventHandler<f64>
+}
+
 #[component]
-pub fn Number() -> Element {
+pub fn Number(props: NumberProps) -> Element {
     let styles: String = use_hook(|| get_asset!("/assets/styles/ui/number.css"));
+
+    let input_handler = move |evt: Event<FormData>| {
+        let min_ref = props.min.read();
+        let min = min_ref.deref().clone();
+        let max_ref = props.max.read();
+        let max = max_ref.deref().clone();
+        let new_value: f64 = evt.data.value().parse().unwrap_or(min);
+        if new_value < min {
+            props.oninput.call(min);
+        } else if new_value > max {
+            props.oninput.call(max);
+        } else {
+            props.oninput.call(new_value);
+        };
+    };
+
+    let increase_handler = move |_: Event<MouseData>| {
+        let min_ref = props.min.read();
+        let min = min_ref.deref().clone();
+        let max_ref = props.max.read();
+        let max = max_ref.deref().clone();
+        let step = props.step.read();
+        let step = step.deref().clone();
+        if props.value.read().deref() + step > max {
+            props.oninput.call(max);
+        } else if props.value.read().deref() + step < min {
+            props.oninput.call(min);
+        } else {
+            let cv = props.value.read().deref().clone();
+            let result_float = cv + step;
+            let formatted_result = (result_float * 100.0).round() / 100.0;
+            props.oninput.call(formatted_result);
+        }
+    };
+
+    let decrease_handler = move |_: Event<MouseData>| {
+        let min_ref = props.min.read();
+        let min = min_ref.deref().clone();
+        let max_ref = props.max.read();
+        let max = max_ref.deref().clone();
+        let step = props.step.read();
+        let step = step.deref().clone();
+        if props.value.read().deref() - step > max {
+            props.oninput.call(max);
+        } else if props.value.read().deref() - step < min {
+            props.oninput.call(min);
+        } else {
+            let cv = props.value.read().deref().clone();
+            let result_float = cv - step;
+            let formatted_result = (result_float * 100.0).round() / 100.0;
+            props.oninput.call(formatted_result);
+        }
+    };
+
     rsx! {
         div { class: "ui-number",
             document::Stylesheet { href: "{styles}" }
 
-            button { class: "ui-number__step-button left", "<" }
-            input { class: "ui-number__input", r#type: "number" }
-            button { class: "ui-number__step-button right", ">" }
+            button {
+                class: "ui-number__step-button left",
+                onclick: decrease_handler,
+                "<"
+            }
+            input {
+                class: "ui-number__input",
+                r#type: "number",
+                min: "{props.min}",
+                max: "{props.max}",
+                value: "{props.value}",
+                step: "{props.step}",
+                oninput: input_handler,
+            }
+            button {
+                class: "ui-number__step-button right",
+                onclick: increase_handler,
+                ">"
+            }
         }
     }
 }
