d0d625903ff014e1151286c2dc134378fd3b5071
keiko37 <81381572+keiko37@users.noreply.github.com>
2025-05-05 21:48:47 +0300

    removed nvidia webkit3gtk fix from dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 # Fixes bug with content visibility on machines with nvidia cards
-WEBKIT_DISABLE_DMABUF_RENDERER=1 TEST_PLUGIN_PATH=../nmp-settings-plugin-example/target/debug/nmp-settings-plugin-example dx serve
+TEST_PLUGIN_PATH=../nmp-settings-plugin-example/target/debug/nmp-settings-plugin-example dx serve
