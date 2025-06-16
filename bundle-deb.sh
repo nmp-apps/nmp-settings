f946247b74d0ea3b518f62b0e70183970fb285ed
keiko37 <81381572+keiko37@users.noreply.github.com>
2025-06-17 01:37:18 +0300

    added new app icon sizes, changed bundle short descsription, some other minor changes

--- bundle-deb.sh
+++ bundle-deb.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-# STRIP doesn't work on all linux distributions
+# STRIP doesn't work on some linux distributions
 NO_STRIP=true dx bundle --platform linux --package-types "deb" --verbose --release
---
+++ bundle-default.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+# STRIP doesn't work on some linux distributions
+NO_STRIP=true dx bundle --platform linux --verbose --release
